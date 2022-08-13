a305f2575df676f6486c8c644f965d86161717be
kate415 <katenish415@gmail.com>
2022-08-13 22:57:05 +0900

    customize header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,48 @@
 import Link from 'next/link'
 
+const Grass = () => {
+  const grassColors: string[] = [
+    'bg-green-500',
+    'bg-green-600',
+    'bg-green-700',
+    'bg-green-800',
+    'bg-green-900',
+  ]
+  const grassColor: string =
+    grassColors[Math.floor(Math.random() * grassColors.length)]
+  return <div className={`w-2 h-2 ${grassColor}`} />
+}
+
 const Header = () => {
+  var grasses = []
+  const cell: number = 625
+  const row: number = 15
+  for (var i = 0; i < row; i++) {
+    var tmp = []
+    for (var _ = 0; _ < cell; _++) {
+      tmp.push(<Grass />)
+    }
+    grasses.push(tmp)
+  }
+
   return (
-    <header>
-      <nav className="flex flex-rows bg-green-500 px-10 md:px-20 py-10 text-black font-bold text-2xl">
+    <header className='flex flex-row min-w-max overflow-hidden relative'>
+      <div className='flex flex-col'>
+        {grasses.map((rows, rowsIndex) => {
+          return (
+            <div key={rowsIndex} className='flex flex-row'>
+              {rows.map((cell, cellIndex) => {
+                return <div key={cellIndex}>{cell}</div>
+              })}
+            </div>
+          )
+        })}
         <Link href='/'>
-          <a>けいとくらふと情報局</a>
+          <a className='text-white font-bold text-2xl absolute px-10 md:px-20 py-10'>
+            けいとくらふと情報局
+          </a>
         </Link>
-      </nav>
+      </div>
     </header>
   )
 }
