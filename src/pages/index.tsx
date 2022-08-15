54bdc2d4d0b1c86e60a0aafb85772e5e5d5b81e3
kate415 <katenish415@gmail.com>
2022-08-15 11:26:29 +0900

    add microcms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <div>Hello World!</div>
+import Link from 'next/link'
+import { client } from '../libs/client'
+import type { Blogs, Blog } from '../types/blogs'
+
+type Props = {
+  blogs: Blog[]
+}
+
+export default function Home({ blogs }: Props) {
+  return (
+    <div>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blogs/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps = async() => {
+  const blogs: Blogs = await client.get({ endpoint: 'blogs' })
+  return {
+    props: {
+      blogs: blogs.contents,
+    }
+  }
 }
---
+++ src/types/blogs.ts
@@ -0,0 +1,33 @@
+export type Eyecatch = {
+  url: string
+  height: number
+  width: number
+}
+
+export type Category = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  name: string
+}
+
+export type Blog = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  content: string
+  eyecatch: Eyecatch
+  category: Category
+}
+
+export type Blogs = {
+  contents: Blog[]
+  totalCount: number
+  offset: number
+  limit: number
+}
